dba5a57a3564aa93fe6aafac1f79fd67ec6f59c8
jeffalo <40470736+jeffalo@users.noreply.github.com>
2021-03-22 18:29:33 +0100

    add vue-good-table and demo page

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/auth.js'],
+  plugins: ['~/plugins/auth.js', { src: '~/plugins/vue-good-table', ssr: false }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
