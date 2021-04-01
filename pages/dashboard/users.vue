a4f6bf3d7c4ae4d493a92614594059599b205c96
jeffalo <40470736+jeffalo@users.noreply.github.com>
2021-04-01 09:10:37 +0200

    add status field to userlist

--- pages/dashboard/users.vue
+++ pages/dashboard/users.vue
@@ -28,6 +28,10 @@ export default {
           field: 'name',
         },
         {
+          label: 'status',
+          field: 'status',
+        },
+        {
           label: 'color',
           field: 'color'
         },
