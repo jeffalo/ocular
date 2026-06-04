23d879ec35f3eabf83758455a3d6e91e03d96a96
John <roblox888i12345@gmail.com>
2026-06-04 16:08:46 -0400

    add more forums

--- assets/category-map.js
+++ assets/category-map.js
@@ -62,5 +62,8 @@ export default new Map(
         ["59", "فارسی"],
         ["60", "Project Save & Level Codes"],
         ["61", "April Fools Day - Suggest-Show-Question-Bugs-Help-Glitch-Tell-Etc"],
+        ["62", "हिन्दी (Hindi)"],
+        // ["63", "403, unkown"],
+        ["64", "Youth Advisory Board"],
     ]
 );
